e79cdbb08593c885f4c673630d74efc65d3d3004
LuborRod <rodionliuborets@gmail.com>
2021-04-28 19:49:00 +0300

    route + controller

--- app/Http/Controllers/Api/Transaction/TransactionController.php
+++ app/Http/Controllers/Api/Transaction/TransactionController.php
@@ -1,7 +1,14 @@
 <?php
 
+namespace App\Http\Controllers\Api\Transaction;
 
-class TransactionController
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
 {
-
+    public function create(Request $request)
+    {
+        var_dump(123);
+    }
 }
