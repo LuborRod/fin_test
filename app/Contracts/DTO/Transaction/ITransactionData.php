2fc7bcd0f743696721e722fb3ef73ca65732d14a
LuborRod <rodionliuborets@gmail.com>
2021-05-05 22:20:59 +0300

    refactor

--- app/Contracts/DTO/Transaction/ITransactionData.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace App\Contracts\DTO\Transaction;
-
-interface ITransactionData
-{
-    public function create(string $senderWalletHash, string $receiverWalletHash, int $amount, ?int $commissionPayer): void;
-}
--- app/Contracts/DTO/TransferSums/ITransferSumsData.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Contracts\DTO\TransferSums;
-
-interface ITransferSumsData
-{
-    public function create(int $sender, int $receiver, int $commission): void;
-}
